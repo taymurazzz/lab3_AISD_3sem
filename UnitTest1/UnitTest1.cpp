3ffdd0e5a42845d5f5d60149240cc47e35f4b81c
taymurazzz <75258290+taymurazzz@users.noreply.github.com>
2021-12-02 03:20:53 +0300

    Update UnitTest1.cpp

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -38,5 +38,43 @@ namespace UnitTest1
 			a.remove(2);
 			Assert::IsTrue(a.at(1) == 3);
 		}
+		TEST_METHOD(iteratorS_test)
+		{
+			heap a;
+			a.begin();
+			a.insert(1);
+			a.insert(2);
+			a.insert(3);
+			a.insert(4);
+			a.insert(5);
+			iteratorS* a_iteratorS = a.create_iteratorS();
+			int i = 1;
+			while (a_iteratorS->has_next()) {
+				Assert::IsTrue((a_iteratorS->next())==i);
+					i++;
+			}
+		}
+		TEST_METHOD(iteratorG_test)
+		{
+			heap a;
+			a.begin();
+			a.insert(1);
+			a.insert(2);
+			a.insert(3);
+			a.insert(4);
+			a.insert(5);
+			iteratorG* a_iteratorG = a.create_iteratorG();
+			int masss[6];
+			masss[0] = 1;
+			masss[1] = 2;
+			masss[2] = 4;
+			masss[3] = 5;
+			masss[4] = 3;
+			int ii = 0;
+			while (ii < a.size()) {
+				Assert::IsTrue((a_iteratorG->Next()) == masss[ii]);
+				ii++;
+			}
+		}
 	};
 }
